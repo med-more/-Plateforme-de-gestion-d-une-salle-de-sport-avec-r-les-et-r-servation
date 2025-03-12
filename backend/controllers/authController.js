73f83941f23d68a98b3525b5e5ca4cfaeff31a9b
ENAA <khadijauser779@gmail.com>
2025-03-12 14:10:22 +0100

    add authControllers

---
+++ backend/controllers/authController.js
@@ -0,0 +1,37 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, role } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    user = new User({ firstName, lastName, email, password, role });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+    res.json({ token, message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
